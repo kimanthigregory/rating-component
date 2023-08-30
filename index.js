46e086cd4ef55be9c44fb929020e9e3459e6153e
Greg <kimanthigregory>
2023-08-30 22:32:01 +0300

    romoved some unanted  code in the javasxript file

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-alert("hello");
-// var ratingStarContainer= document.querySelectorAll(".rating-star");
-// console.log(ratingStarContainer);
 var ratingStar = document.querySelector(".rating-star");
 for(var i =0; i<document.querySelectorAll(".rating-num").length; i++){
     document.querySelectorAll(".rating-num")[i].addEventListener("click",function(){
         var ratingNum = parseInt(this.getAttribute("data-ratingNum"))
-        console.log(ratingNum);
-        // document.querySelector(".rating-star").classList.add("add-rating-star");
         userRating(ratingNum);
         document.querySelector(".selection p span").textContent = ratingNum
 
@@ -27,5 +22,4 @@ function userRating(ratingNumber) {
 document.querySelector(".submit-button button").addEventListener("click",function submitButton(userChosenButton){
     document.querySelector(".thank-you-container").classList.add("add-container")
     document.querySelector(".rating-container").classList.add("remove-container")
-    // console.log(userChosenButton);
 })
